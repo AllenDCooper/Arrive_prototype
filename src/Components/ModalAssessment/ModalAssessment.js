65d0daaad43567e817a29ea18becf4e6afdab0c6
ADC04 <allendcooper@gmail.com>
2022-02-10 11:39:36 -0500

    Added ACES modal to institutional course space

---
+++ src/Components/ModalAssessment/ModalAssessment.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import { Modal, Button, Card, Form, ProgressBar, Row, Col } from 'react-bootstrap';
+import itemsArr from '../../ACES_Assessment/itemsArr';
+import FormCheck from '../FormCheck/FormCheck';
+
+class ModalAssessment extends Component {
+
+  state = {
+    show: 0,
+    setShow: 0,
+    validated: false,
+  }
+
+  handleOpen = () => {
+    this.setState({
+      show: 1,
+      setShow: 1
+    })
+  }
+
+  setValidated = (boolean) => {
+    this.setState({
+      validated: boolean
+    })
+  }
+
+  handleCancel = () => {
+    this.setState({
+      show: 0,
+      setShow: 0
+    })
+  }
+
+  handleSubmit = (event) => {
+    // checks to see if all items are answered
+    this.setValidated(false);
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.setValidated(true)
+    } else {
+      event.preventDefault();
+      this.setValidated(true)
+
+      // checks to see if it is the final modal of the assessment
+      // if it is, it will run submitScore function
+      // else it will advance to next modal
+      const numPages = Math.ceil(itemsArr.length / 10)
+      if (this.state.show === numPages) {
+        this.props.submitScore();
+        console.log("score submitted");
+        this.setValidated(false)
+      } else {
+        this.setValidated(false)
+        this.setState(state => {
+          const show = state.show + 1;
+          const setShow = state.setShow + 1;
+          return {
+            show,
+            setShow
+          };
+        });
+      }
+    }
+  }
+
+  // function that takes an array and breaks it down into smaller arrays limited to 10 items each
+  createChunkedArrays = (arr, size) => {
+    const chunked_arr = [];
+    let copied = [...arr];
+    const numOfChildArr = Math.ceil(copied.length / size);
+    for (let i = 0; i < numOfChildArr; i++) {
+      chunked_arr.push(copied.splice(0, size));
+    }
+    return chunked_arr;
+  }
+
+  // render function dynamically creates forms for each chunked array
+  render() {
+    console.log(this.state.validated);
+    const chunkedArrays = this.createChunkedArrays(itemsArr, 10)
+    return (
+      <div>
+        <Card variant="primary" onClick={this.handleOpen}>
+          <Row style={{margin: '0px'}}>
+            <Col xs={2} style={{position: 'relative', padding: '0px'}}>
+              <img style={{width: '100%', maxWidth: '100px', margin: '5px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}} alt='ACES logo' src='/aces.png'/>
+            </Col>
+            <Col xs={8}>
+              <Card.Body>
+                <Card.Title style={{fontSize: '14px'}}>ACES Strengths Assessment</Card.Title>
+                <Card.Subtitle style={{fontSize: '12px'}} className="mb-2 text-muted">Discover your strengths and growth areas</Card.Subtitle>
+                <Card.Text style={{fontSize: '12px'}}>
+                  This 20-minute self-assessment has no right answers and no wrong answers. It is designed by experts in counseling psychology to help you identify your inner assets, grow new ones, and overcome your obstacles.
+                </Card.Text>
+              </Card.Body>
+            </Col>
+            <Col xs={1}>
+
+            </Col>
+          </Row>
+        </Card>
+        {chunkedArrays.map((item, index) => (
+          <Modal size="xl" key={`key-${index}`} show={this.state.show === (index + 1)} onHide={this.handleCancel}>
+            <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  <span className='modal-form-header'>ACES Inventory</span>
+                  <div style={{ display: 'inline' }}><ProgressBar style={{ marginTop: '4px', height: '1.5rem' }} now={100 * (index / (Math.ceil(itemsArr.length / 10)))} srOnly /></div>
+                  <br></br>
+                  Questions (page {index + 1} of {Math.ceil(itemsArr.length / 10)})
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <FormCheck itemsPageArr={item} updateScore={this.props.updateScore} pageNum={index} />
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.props.randomScore}>
+                  Random Score
+                </Button>
+                <Button variant="secondary" onClick={this.handleCancel}>
+                  Cancel
+                </Button>
+                <Button variant="primary" type="submit">
+                  Next
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal>
+        ))
+        }
+      </div>
+    )
+  }
+}
+
+export default ModalAssessment 
--- src/Pages/InstitutionalSpace/InstitutionalSpace.js
+++ src/Pages/InstitutionalSpace/InstitutionalSpace.js
@@ -1,17 +1,118 @@
 import React, { Component } from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Button, Accordion, Card, Container } from 'react-bootstrap';
 import NavigationBarInCourse from '../../Components/NavigationBarInCourse/NavigationBarInCourse';
 import './InstitutionalSpace.css'
+// importing components
+import ModalAssessment from '../../Components/ModalAssessment/ModalAssessment';
+
+// importing ACES Assessment
+import normTable from '../../ACES_Assessment/normTable.js';
+import itemsArr from '../../ACES_Assessment/itemsArr';
+import scales from '../../ACES_Assessment/scales';
 
 class InstitutionalSpace extends Component {
+
+  state = {
+    takenAssessment: false,
+    spinnerOn: false,
+  }
+
+  // this function will update the answerArr in state each time the user clicks on a radio button to answer an assessment question
+  // it will be passed into ModelAssessment (child) and then into FormCheck (grandchild) as props
+  updateScore = (event, index) => {
+    console.log(event.target.value)
+    console.log(index)
+    const value = parseInt(event.target.value);
+    this.setState(state => {
+      const answerArr = state.answerArr;
+      answerArr[index] = value;
+      return {
+        answerArr
+      }
+    },
+      () => { console.log(this.state.answerArr) }
+    )
+  }
+
+  // this function will fill out the answerArr in state with random answers for each assessment question
+  // it will be passed into ModelAssessment (child) as props and be called when Random Score button is clicked
+  randomScore = () => {
+    this.setState(state => {
+      const answerArr = []
+      // a for loop ensures one random answer will be generated for each question item
+      for (let i = 0; i < itemsArr.length; i++) {
+        // randomly generate a score between 1 and 6 for each item
+        const randomAnswer = Math.floor(Math.random() * 6) + 1
+        answerArr.push(randomAnswer)
+        // console.log(answerArr)
+      }
+      return {
+        answerArr,
+      }
+    },
+      // callback function used to submit score async (after state is updated)
+      () => {
+        this.submitScore()
+        console.log(`answerArr: ${this.state.answerArr}`)
+      }
+    )
+  }
 
   render() {
     console.log(this.props.courseClickChange);
     return (
-      <div className="course-page-wrapper">
-        <NavigationBarInCourse courseClickChange={this.props.courseClickChange}/>
-        <Container style={{padding: '0px'}}>
+      <div className='course-page-wrapper'>
+        <NavigationBarInCourse courseClickChange={this.props.courseClickChange} />
+        <Container style={{ padding: '0px' }}>
         </Container>
+
+        {/* <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Accordion Item #1</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Accordion Item #2</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion> */}
+
+        <div>
+          {/* <Scorecard spinnerOn={this.state.spinnerOn} strengthsArr={this.state.strengthsArr} goalsArr={this.state.goalsArr} /> */}
+          <section>
+            {/* <Accordion as={Card.Header} style={{ marginBottom: "0px" }} eventKey="0">
+                <Card style={{ marginBottom: "30px" }}>
+                    Self-Assessment (Initial)
+                  <Accordion.Collapse eventKey="0">
+                    <div>
+                      <ModalAssessment updateScore={this.updateScore} submitScore={this.submitScore} randomScore={this.randomScore} />
+                      <p>test</p>
+                    </div>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion> */}
+
+            <ModalAssessment updateScore={this.updateScore} submitScore={this.submitScore} randomScore={this.randomScore} />
+
+          </section>
+        </div>
+
       </div>
     )
   }
