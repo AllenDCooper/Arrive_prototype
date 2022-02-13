eee779cb441aa692880d86e96215dd4313ad4b49
ADC04 <allendcooper@gmail.com>
2022-02-13 00:51:24 -0500

    added floating header and footer on connect and institutional space to handle chat message feed

--- src/Components/NavigationBarInCourse/NavigationBarInCourse.js
+++ src/Components/NavigationBarInCourse/NavigationBarInCourse.js
@@ -10,7 +10,6 @@ function NavigationBarInCourse(props) {
   console.log(props)
 
   const handleClick = (targetName) => {
-    event.preventDefault();
     console.log('clicked');
     console.log(targetName)
     props.changeTab(targetName);
@@ -50,48 +49,51 @@ function NavigationBarInCourse(props) {
     //   </Navbar.Collapse>
     // </Navbar>
     <div>
-      <Navbar expand={false} style={{ backgroundColor: '#1565c0', color: "#fff" }}>
-        <Container fluid style={{ color: "#fff" }}>
-          <Row style={{ width: '100%', paddingTop: '10px', paddingBottom: '10px', fontWeight: '600', margin: '0px' }}>
-            <Col xs={2}>
-              <span onClick={props.courseClickChange}><img alt='add course icon' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/arrow_carrot-left.svg' style={{ height: '21px', width: '13px' }} /></span>
-            </Col>
-            <Col xs={8} style={{ textAlign: 'center', fontSize: '20px' }}>
-              <Navbar.Brand style={{ color: "#fff", marginRight: '0px' }}>Compass College FYE</Navbar.Brand>
-            </Col>
-            <Col xs={2} style={{ textAlign: 'center', fontSize: '20px' }}>
-            </Col>
-          </Row>
-        </Container>
-      </Navbar>
-      <Navbar style={{ position: 'relative', borderRadius: '0px', backgroundColor: "#064d9e", color: '#fff', height: '60px' }} className="justify-content-center" >
-        <Nav style={{ position: 'absolute', bottom: '0px', minWidth: '300px' }} className="justify-content-center" activeKey="">
-          <Nav.Item name='connect' style={{ width: '25%' }}>
-            <Nav.Link name='connect' className='course-tabs' style={props.tabSelected === 'connect' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('connect')}>
-              <img name='connect' alt='share' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/statistics.svg' className='course-tab-img' />
-              <p name='connect' style={{ marginBottom: '0px' }}>Connect</p>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item style={{ width: '25%' }}>
-            <Nav.Link name='activities' className='course-tabs' style={props.tabSelected === 'activities' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('activities')} >
-              <img name='activities' alt='activities' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/course-history.svg' className='course-tab-img' />
-              <p name='activities' style={{ marginBottom: '0px' }}>Activities</p>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item style={{ width: '25%' }}>
-            <Nav.Link name='goals' className='course-tabs' style={props.tabSelected === 'goals' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('goals')} >
-              <img name='goals' alt='goals' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/study-tools.svg' className='course-tab-img' />
-              <p name='goals' style={{ marginBottom: '0px' }}>Goals</p>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item style={{ width: '25%' }}>
-            <Nav.Link name='resources' className='course-tabs' style={props.tabSelected === 'resources' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('resources')} >
-              <img name='resources' alt='resources' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/assignments.svg' className='course-tab-img' />
-              <p name='resources' style={{ marginBottom: '0px' }}>Resources</p>
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
+      <div style={{ height: '126px' }}></div>
+      <div style={{ position: 'fixed', top: '0px', width: '768px', maxWidth: '100%', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Navbar expand={false} style={{ backgroundColor: '#1565c0', color: "#fff" }}>
+          <Container fluid style={{ color: "#fff" }}>
+            <Row style={{ width: '100%', paddingTop: '10px', paddingBottom: '10px', fontWeight: '600', margin: '0px' }}>
+              <Col xs={2}>
+                <span onClick={props.courseClickChange}><img alt='add course icon' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/arrow_carrot-left.svg' style={{ height: '21px', width: '13px' }} /></span>
+              </Col>
+              <Col xs={8} style={{ textAlign: 'center', fontSize: '20px' }}>
+                <Navbar.Brand style={{ color: "#fff", marginRight: '0px' }}>Compass College FYE</Navbar.Brand>
+              </Col>
+              <Col xs={2} style={{ textAlign: 'center', fontSize: '20px' }}>
+              </Col>
+            </Row>
+          </Container>
+        </Navbar>
+        <Navbar style={{ position: 'relative', borderRadius: '0px', backgroundColor: "#064d9e", color: '#fff', height: '60px' }} className="justify-content-center" >
+          <Nav style={{ position: 'absolute', bottom: '0px', minWidth: '300px' }} className="justify-content-center" activeKey="">
+            <Nav.Item name='connect' style={{ width: '25%' }}>
+              <Nav.Link name='connect' className='course-tabs' style={props.tabSelected === 'connect' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('connect')}>
+                <img name='connect' alt='share' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/statistics.svg' className='course-tab-img' />
+                <p name='connect' style={{ marginBottom: '0px' }}>Connect</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ width: '25%' }}>
+              <Nav.Link name='activities' className='course-tabs' style={props.tabSelected === 'activities' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('activities')} >
+                <img name='activities' alt='activities' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/course-history.svg' className='course-tab-img' />
+                <p name='activities' style={{ marginBottom: '0px' }}>Activities</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ width: '25%' }}>
+              <Nav.Link name='goals' className='course-tabs' style={props.tabSelected === 'goals' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('goals')} >
+                <img name='goals' alt='goals' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/study-tools.svg' className='course-tab-img' />
+                <p name='goals' style={{ marginBottom: '0px' }}>Goals</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ width: '25%' }}>
+              <Nav.Link name='resources' className='course-tabs' style={props.tabSelected === 'resources' ? styles.selectedMenuLink : styles.menuLink} onClick={() => handleClick('resources')} >
+                <img name='resources' alt='resources' src='https://institutional-web-assets-share.s3.amazonaws.com/iClicker/student/images/assignments.svg' className='course-tab-img' />
+                <p name='resources' style={{ marginBottom: '0px' }}>Resources</p>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </div >
     </div>
   )
 }
--- src/Pages/Connect/Connect.js
+++ src/Pages/Connect/Connect.js
@@ -3,21 +3,58 @@ import { Button, Form, Accordion, Card, Container, Row, Col } from 'react-bootst
 import NavigationBarInCourse from '../../Components/NavigationBarInCourse/NavigationBarInCourse';
 
 class Connect extends Component {
+  constructor(props) {
+    super(props);
+    this.messagesEndRef = React.createRef()
+  }
 
+  state = {
+    messageArr: ['new message']
+  }
+
+  componentDidMount() {
+    this.scrollToBottom()
+    setInterval(this.addMessages, 1000)
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom()
+  }
+  scrollToBottom = () => {
+    this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  addMessages = () => {
+    this.setState({
+      messageArr: [...this.state.messageArr, 'new message']
+    })
+    console.log(this.state.messageArr)
+  }
 
   render() {
+    // const { messages } = this.props
     return (
       <div>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+        <div>
+          {this.state.messageArr.map(message => (
+            <div>message</div>
+          ))}
+          {/* {messages.map(message => <Message key={message.id} {...message} />)} */}
+          <div ref={this.messagesEndRef} />
+        </div>
+        <div style={{height: '150px'}}></div>
+        <Form style={{backgroundColor: 'white', position: 'fixed', bottom: '0px', width: '768px', maxWidth: '100%', marginLeft: 'auto', marginRight: 'auto'}}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{marginBottom: '5px'}}>
+            {/* <Form.Label>Add message</Form.Label> */}
+            <Form.Control as="textarea" rows={3} placeholder="Add your thoughts/feelings here..." />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <div style={{textAlign: 'right'}}>
+          <Button variant="primary" type="submit" style={{marginBottom: '10px'}}>
+            Share
           </Button>
+          </div>
         </Form>
-      </div>
+      </div >
     )
   }
 }
